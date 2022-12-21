0428f3d6c2751f00d44524050fa4a7a9b63322d8
Kuca Brice <brice.kuca@etudiant.univ-reims.fr>
2022-12-21 10:34:42 +0100

    ajout d'un marqueur sur la carte

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
+//coordonnées de la map avec le zoom de base
 var map = L.map('map').setView([49.258329, 4.031696], 13);
+
+//ajout de la carte sur la page web
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+//ajout du marqueur
+var marker = L.marker([49.258329, 4.031696]).addTo(map);
