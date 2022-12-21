f3ed3f880bc487f28f5af85cf8b95fadff84bfbe
Kuca Brice <brice.kuca@etudiant.univ-reims.fr>
2022-12-21 10:30:36 +0100

    ajout du script pour afficher la carte a reims en zoom 13

---
+++ script/script.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([49.258329, 4.031696], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
